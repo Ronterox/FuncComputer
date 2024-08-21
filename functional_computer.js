0871664ecb17c295334b6e7345d2a76348399fd2
Ricardo <ricarditomontserrat@gmail.com>
2024-08-21 05:43:16 +0200

    Succesor and exponential on church encoding

--- functional_computer.js
+++ functional_computer.js
@@ -1,23 +1,21 @@
 const printLn = x => console.log(x)
 
 const toNumber = n => n (i => i + 1) (0)
-const toMinus = n => fn => x => n (i => i - 1) (x)
 
 // number = fn => x
 const add = n => m => fn => x => m (fn) (n (fn) (x))
-const sub = n => m => add (n) (toMinus (m))
 const mul = n => m => fn => x => m (n(fn)) (x)
-
-const inc = n => add (n) (one)
-const dec = n => sub (n) (one)
 
 const zero = _ => x => x
 const one = f => x => f (x)
 const two = f => x => f (f (x))
 const three = f => x => f (f (f (x)))
-const fourth = add (two) (two)
+const four = add (two) (two)
 const five = add (three) (two)
 const ten = mul (two) (five)
+
+const succ = add (one)
+const exp = n => m => fn => x => (m (n)) (fn) (x)
 
 const toBool = bool => ifThenElse (bool) (true) (false)
 
@@ -31,4 +29,4 @@ const not = bool => ifThenElse (bool) (False) (True)
 const or = a => b => a (a) (b)
 const and = a => b => a (b) (a)
 
-printLn (toNumber (sub (inc (ten)) (dec (fourth) )))
+printLn (toNumber (exp (two) (five)))
